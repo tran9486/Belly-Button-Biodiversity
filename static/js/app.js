63234ea2546750488f576638b267bd3c3a676157
Christopher Tran <ChristopherTran94@gmail.com>
2021-06-28 18:45:06 -0700

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -24,6 +24,7 @@ function init () {
                         size:sample_values
                     }
                 }]
+
                 Plotly.newPlot("bubble", bubbleData)
 
                 var newIds = [];
